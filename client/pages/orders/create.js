f2550ef0236b1fcdcae8ae72b15e3bea6a514ead
Gyucheon Heo <gyucheon.heo@gmail.com>
2020-03-18 01:13:05 -0500

    Add createorder page

---
+++ client/pages/orders/create.js
@@ -0,0 +1,221 @@
+import React from 'react'
+import Layout from '../../components/MyLayout'
+import Sidebar from '../../components/Sidebar';
+import Alert from '../../components/Alert'
+import axios from 'axios'
+import Router from 'next/router';
+
+export default class Createorder extends React.Component {
+    state = {
+        pid:0,
+        state:'' ,
+        plate:'',
+        type:'',
+        name:'',
+        email:'',
+        card:'',
+        name_on_card:'',
+        expiration_date_month: '',
+        expirmation_date_year:'',
+        zip:0,
+        errorMessage:'',
+        parks_data: []
+    }
+
+    componentDidMount(){
+        axios.get(`http://localhost:8080/parkpay/parks`)
+        .then(res => {
+            this.setState({
+                parks_data : res.data 
+            })
+        })
+    }
+    handleChange = event => {
+        this.setState({ [event.target.name] : event.target.value });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        axios.post(`http://localhost:8080/parkpay/orders`,
+        {            
+            pid : this.state.pid,
+            vehicle:{
+               state: this.state.state,
+               plate: this.state.plate,
+               type: this.state.type
+            },
+            visitor:{
+                name: this.state.name,
+                email: this.state.email,
+                payment_info:{
+                    card: this.state.card,
+                    name_on_card : this.state.name_on_card,
+                    expiration_date: this.state.expiration_date_month+"/"+this.state.expiration_date_year,
+                    zip: this.state.zip
+                }
+            }
+        })
+        .then(res => {
+            // redirect...!
+            Router.push('/orders');
+       })
+        .catch(err => {
+            if(err.response.status == 400){
+                this.setState({
+                    errorMessage : err.response.data.title
+                })
+            }
+        })
+    }
+
+    render(){
+        const list_of_states = [
+            "AK",
+            "AL",
+            "AR",
+            "AZ",
+            "CA",
+            "CO",
+            "CT",
+            "DE",
+            "FL",
+            "GA",
+            "HI",
+            "IA",
+            "ID",
+            "IL",
+            "IN",
+            "KS",
+            "KY",
+            "LA",
+            "MA",
+            "MD",
+            "ME",
+            "MI",
+            "MN",
+            "MO",
+            "MS",
+            "MT",
+            "NC",
+            "ND",
+            "NE",
+            "NH",
+            "NJ",
+            "NM",
+            "NV",
+            "NY",
+            "OH",
+            "OK",
+            "OR",
+            "PA",
+            "RI",
+            "SC",
+            "SD",
+            "TN",
+            "TX",
+            "UT",
+            "VA",
+            "VT",
+            "WA",
+            "WI",
+            "WV",
+            "WY",
+         ]
+        return(
+            <Layout>
+                <div className="row">
+                    <div className="col-md-7">
+                    <h3>Create New Order</h3>
+                    <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="exampleinputemail1">park id</label>
+                        <select className="form-control" name="pid" value={this.state.pid} onChange={this.handleChange} placeholder="Choose Park">
+                            <option key="default" default>Select</option>
+                            {this.state.parks_data.map(park => (
+                                <option key={park.pid} value={park.pid}>{park.location_info.name},{park.location_info.region}</option>
+                            ))}
+                        </select>
+                        <small id="emailhelp" className="form-text text-muted">required</small>
+                    </div>
+                    <h4> Vehicle Information</h4>
+                    <div className="form-group">
+                        <label htmlFor="exampleinputemail1">state</label>
+                        <select className="form-control" name="state" value={this.state.state} onChange={this.handleChange} placeholder="Choolse State on the plate">
+                            <option key="default" default>Select</option>
+                            {list_of_states.map(abbr => 
+                                (<option key={abbr} value={abbr}>{abbr}</option>))}
+                        </select>
+                        <small id="emailhelp" className="form-text text-muted">required.</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleinputemail1">plate</label>
+                        <input className="form-control" type="text" name="plate" value={this.state.plate} onChange={this.handleChange} />
+                        <small id="emailhelp" className="form-text text-muted">required.</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleinputemail1">type</label>
+                        <select className="form-control" onChange={this.handleChange} name="type" value={this.state.type} placeholder="Choose a type of vehicle">
+                            <option key="default" default>Select </option>
+                            <option value="car">Car</option>
+                            <option value="rv">RV</option>
+                            <option value="motorcycle">Motorcycle</option>
+                        </select>
+                        <small id="emailhelp" className="form-text text-muted">required.</small>
+                    </div>
+                    <h4> Visitor Information</h4>
+                    <div className="form-group">
+                        <label htmlFor="exampleinputemail1">Name</label>
+                        <input className="form-control" type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+                        <small id="emailhelp" className="form-text text-muted">optional.</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleinputemail1">E-mail</label>
+                        <input className="form-control" type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+                        <small id="emailhelp" className="form-text text-muted">required.</small>
+                   </div>
+
+                    <h4> Payment Information</h4>
+                    <div className="form-group">
+                        <label htmlFor="inputpassword4">Card Number</label>
+                        <input className="form-control" type="text" name="card" value={this.state.card} onChange={this.handleChange} />
+                        <small id="emailhelp" className="form-text text-muted">required.</small>
+                    </div>
+
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label htmlFor="exampleinputemail1">Name on Card</label>
+                            <input className="form-control" type="text" name="name_on_card" value={this.state.name_on_card} onChange={this.handleChange} />
+                            <small id="emailhelp" className="form-text text-muted">required</small>
+                        </div>
+                            <div className="form-group col-md-3">
+                                <label htmlFor="exampleInputEmail1">Exp Date-Month</label>
+                                <input className="form-control" type="text" name="expiration_date_month" value={this.state.expiration_date_month} onChange={this.handleChange} />
+                                <small id="emailHelp" className="form-text text-muted">Required</small>
+                            </div>
+                            <div className="form-group col-md-3">
+                                <label htmlFor="exampleInputEmail1">Exp Date-Year</label>
+                                <input className="form-control" type="text" name="expiration_date_year" value={this.state.expiration_date_year} onChange={this.handleChange} />
+                                <small id="emailHelp" className="form-text text-muted">Required</small>
+                            </div>
+                    </div>
+
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label htmlFor="exampleInputEmail1">zip</label>
+                            <input className="form-control" type="text" name="zip" value={this.state.zip} onChange={this.handleChange} />
+                            <small id="emailHelp" className="form-text text-muted">Required</small>
+                        </div>
+                    </div>
+                   { this.state.errorMessage &&
+                        <Alert type="warning" text={this.state.errorMessage} />
+                    }
+                    <input type="submit" className="btn btn-primary btn-lg btn-block" value="Place Order"></input>
+                    </form>
+                </div> 
+                <Sidebar />
+                </div>
+          </Layout>
+        )
+    }
+
+}
