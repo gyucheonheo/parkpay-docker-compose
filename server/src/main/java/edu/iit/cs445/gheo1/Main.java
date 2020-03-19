c3d5c0f6fbcd1d3dd993b718f79b21f5bd5d82a3
Gyucheon Heo <gyucheon.heo@gmail.com>
2020-03-19 16:44:39 -0500

    Webserver thread update

--- server/src/main/java/edu/iit/cs445/gheo1/Main.java
+++ server/src/main/java/edu/iit/cs445/gheo1/Main.java
@@ -15,6 +15,7 @@ public class Main {
     // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/parkpay/";
 
+    	
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
@@ -36,10 +37,16 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
+        try {
+        	server.start();
+        	Thread.currentThread().join();
+        } catch(Exception e) {
+        	System.err.println(e);
+        } finally {
+        	server.stop();
+        }
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
     }
 }
 
